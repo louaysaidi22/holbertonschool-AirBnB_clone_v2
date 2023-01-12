99a41e364ccca85e94d7ffc50611efe1bdd53779
louaysaidi22 <louaysaidi18@gmail.com>
2023-01-12 12:40:18 +0100

    third task*

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -15,5 +15,10 @@ def hbnb_route():
     return "HBNB"
 
 
+@app.route("/c/<text>", strict_slashes=False)
+def c_route():
+    return "C " + str(text.replace('_', ' '))
+
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
